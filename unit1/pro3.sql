b76a634ae84c4da8398c182f3fa0de8770624914
krisha1631 <krisha.bhakhar136888@marwadiuniversity.ac.in>
2026-08-11 15:09:04 +0530

    Update pro3.sql

--- unit1/pro3.sql
+++ unit1/pro3.sql
@@ -1,9 +1,24 @@
---number., Write a PL/SQL block to accept product name, qty and
---price from user and then calculate discount in Rs. based on the
---given (%).,
+SET SERVEROUTPUT ON;
 
-set serveroutput on
-declare 
-begin
-end;
+DECLARE
+    pname VARCHAR2(30) := '&product_name';
+    qty NUMBER := &quantity;
+    price NUMBER := &price;
+    per NUMBER := &discount_percent;
+    total NUMBER;
+    discount NUMBER;
+    final_amount NUMBER;
+
+BEGIN
+    total := qty * price;
+    discount := total * per / 100;
+    final_amount := total - discount;
+
+    DBMS_OUTPUT.PUT_LINE('Product Name = ' || pname);
+    DBMS_OUTPUT.PUT_LINE('Quantity = ' || qty);
+    DBMS_OUTPUT.PUT_LINE('Price = ' || price);
+    DBMS_OUTPUT.PUT_LINE('Total Amount = ' || total);
+    DBMS_OUTPUT.PUT_LINE('Discount = Rs. ' || discount);
+    DBMS_OUTPUT.PUT_LINE('Final Amount = Rs. ' || final_amount);
+END;
 /
